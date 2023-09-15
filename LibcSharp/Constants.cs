bd1165eb683ec11a86606b4a6f035912dfcede94
Massoud Asadi <massoud.asadi@hotmail.com>
2023-09-15 19:39:05 +0330

    SIGKILL && SIGTERM added

---
+++ LibcSharp/Constants.cs
@@ -0,0 +1,10 @@
+namespace LibcSharp;
+public static class Constants {
+
+    // Signals
+    public static readonly int SIGKILL = 9;
+    public static readonly int SIGTERM = 15;
+
+    // Ethernet Consts
+    public const int ETH_ALEN = 6;
+}
