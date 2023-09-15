e40ad587bc6cda100cfba4b779d403a4cf765796
Massoud Asadi <massoud.asadi@hotmail.com>
2023-09-15 19:40:13 +0330

    sample update

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -2,3 +2,6 @@
 
 int pid = Libc.getpid();
 Console.WriteLine($"Current process ID: {pid}");
+
+int result = Libc.kill(pid, (int)Signal.KILL); // or you can use Constants.SIGKILL
+Console.WriteLine($"Kill result: {result}");
